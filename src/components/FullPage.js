77657218ff1a647f4f290d66a8268b2606a92169
JUDGEPM20 <pjudge2002@gmail.com>
2023-03-06 19:07:44 -0500

    finished the page (added linked to the buttons)

--- src/components/FullPage.js
+++ src/components/FullPage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './FullPage.css'
 import OrderForm from '../components/OrderForm.js';
+import ScrollIntoView from 'react-scroll-into-view';
+
+
 const FullPage = (props) => {
 
     return (
@@ -10,12 +13,12 @@ const FullPage = (props) => {
                     <div className='subtitle'>BASED IN GROVE CITY, PA</div>
                     <div id='cover-text'>IT'S BANANA BREAD</div>
                     <div className='text'>Inspiring the comfort and memories of homebaking in the lives of college students.</div>
-                    <a>
+                    <ScrollIntoView selector='#section4'>
                         <div className='br-pill shadow grow-large' id='landing-btn'>
                             <span className='subtitle'>ORDER NOW</span>
                             <i className="fa-solid fa-arrow-down" id='fa-arrow-btn1'></i>
                         </div>
-                    </a>
+                    </ScrollIntoView>
                 </div>
             </div>
             <div className='row' id='section1'>
@@ -24,10 +27,12 @@ const FullPage = (props) => {
                         <div className='subtitle '>COULD YOU IMAGINE ANY OTHER WAY?</div>
                         <div className='heading'>BAKED FRESH</div>
                         <div className='text'>This banana bread, there's nothing like it. We could go into how we make it with only 9 ingredients you can find at your local grovery, but you'll know that immediately when you try it.</div>
-                        <span className='fa-stack fa-2x grow-large'>
-                            <i className="fa-solid fa-circle fa-stack-2x" id='fa-circle-btn2'></i>
-                            <i className="fa-inverse fa-solid fa-arrow-down fa-stack-1x" id='fa-arrow-btn2'></i>
-                        </span>
+                        <ScrollIntoView selector='#section2'>
+                            <span className='fa-stack fa-2x grow-large'>
+                                <i className="fa-solid fa-circle fa-stack-2x" id='fa-circle-btn2'></i>
+                                <i className="fa-inverse fa-solid fa-arrow-down fa-stack-1x" id='fa-arrow-btn2'></i>
+                            </span>
+                        </ScrollIntoView>
                     </div>
                 </div>
                 <div className='col-lg-6' id='body-img-con1'><img className='site-img br4' width='50' src={require('../images/bananabreadinoven.jpg')} /></div>
@@ -39,10 +44,12 @@ const FullPage = (props) => {
                         <div className='subtitle '>SEE EM' IN THE BACK THERE?</div>
                         <div className='heading'>REAL BANANAS</div>
                         <div className='text'>That's how Grandma did it, that's how we do it. Years of baking experience, we know what the best bananas look like, trust us.</div>
-                        <span className='fa-stack fa-2x grow-large'>
-                            <i className="fa-solid fa-circle fa-stack-2x" id='fa-circle-btn3'></i>
-                            <i className="fa-inverse fa-solid fa-arrow-down fa-stack-1x" id='fa-arrow-btn3'></i>
-                        </span>
+                        <ScrollIntoView selector='#section3'>
+                            <span className='fa-stack fa-2x grow-large'>
+                                <i className="fa-solid fa-circle fa-stack-2x" id='fa-circle-btn3'></i>
+                                <i className="fa-inverse fa-solid fa-arrow-down fa-stack-1x" id='fa-arrow-btn3'></i>
+                            </span>
+                        </ScrollIntoView>
                     </div>
                 </div>
             </div>
@@ -54,12 +61,12 @@ const FullPage = (props) => {
                     <div className='center-vert'> {/* body-text-con */}
                         <div className='subtitle'>IT'S BANANA BREAD, DELIVERED</div>
                         <div className='text'>We don't blame you for staying inside. Let us freeze so we can deliver your fresh, warm banana bread. Plus, we can always get new delivery people.</div>
-                        <a>
+                        <ScrollIntoView selector='#section4'>
                             <div className='grow-large w-50 m-auto'>
                                 <span className='subtitle ibb-c'>ORDER NOW</span>
                                 <i className="fa-solid fa-arrow-down ibb-c" id='fa-arrow-btn1'></i>
                             </div>
-                        </a>
+                        </ScrollIntoView>
                     </div>
                 </div>
             </div>

6258623055acae5e3d80a924ffc69d57f47d23e1
JUDGEPM20 <pjudge2002@gmail.com>
2023-04-17 20:37:47 -0400

    small styling changes

--- src/components/FullPage.js
+++ src/components/FullPage.js
@@ -73,7 +73,8 @@ const FullPage = (props) => {
             <div className='' id='section4'>
                 <hr className='page-line' />
                 {/* <OrderForm id='page-form' /> */}
-                <div className='subtitle'>We will not be accepting orders this week! Please come back next week!</div>
+                <div className='heading'>Please come back next week!</div>
+                <div className='subtitle'>We will not be accepting orders this week!</div>
                 <hr className='page-line margin-7-top' />
                 <div className='subtitle'>PAYMENT INFO</div>
                 <div><a href='https://account.venmo.com/u/Its-BananaBread'><img className='br4' id='venmo-img' src={require('../images/ibbvenmoQR.png')} /></a></div>

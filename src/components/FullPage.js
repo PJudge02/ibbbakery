d6ed380d379d1fa27a6b8549a6a2e18c6b58060f
JUDGEPM20 <pjudge2002@gmail.com>
2023-10-30 18:12:18 -0400

    Closing form

--- src/components/FullPage.js
+++ src/components/FullPage.js
@@ -74,9 +74,9 @@ const FullPage = (props) => {
                 <hr className='page-line' />
                 <div className='heading'>Deliveries on Tuesday</div>
                 <div className='subtitle'>Place order any day of the week!</div>
-                <OrderForm id='page-form' />
-                {/* <div className='heading'>We have closed shop for the summer!</div>
-                <div className='subtitle'>Please come back next week! (Checkout our instagram or telegram to stay informed!)</div> */}
+                {/* <OrderForm id='page-form' /> */}
+                { <div className='heading'>We have closed shop for the Semester!</div>}
+                {/* <div className='subtitle'>Please come back next week! (Checkout our instagram or telegram to stay informed!)</div> */} */}
                 <hr className='page-line margin-7-top' />
                 <div className='subtitle'>PAYMENT INFO</div>
                 <div><a href='https://account.venmo.com/u/Its-BananaBread'><img className='br4' id='venmo-img' src={require('../images/ibbvenmoQR.png')} /></a></div>
